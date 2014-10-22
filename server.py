8291ceab413e155d5fc4e65dd94efb41e0542817
Arin Saghatelian <arin.saghatelian@telescope.tv>
2014-10-22 13:25:46 -0700

    adding in basic files for heroku

--- server.py
+++ server.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python
+import os
 from Email import Email
 
-Email.run(debug = True)
+Email.config.update(
+    DEBUG = False,
+)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    Email.run(host='0.0.0.0', port=port)
