32e9ee8e19501b858c8840e10aecc98d54b36953
Arin Saghatelian <arin.saghatelian@telescope.tv>
2014-10-22 13:40:48 -0700

    updated port

--- server.py
+++ server.py
@@ -7,5 +7,5 @@ Email.config.update(
 )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 33507))
     Email.run(host='0.0.0.0', port=port)
